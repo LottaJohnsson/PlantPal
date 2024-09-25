2c8abccf910c11e4bf9ad93cdcd8d10bc83f1c15
Charlotta Johnsson <chjohns@kth.se>
2024-09-25 17:16:42 +0200

    Fix #1 Imperfect but a start of implementing topbar and color palette

--- client/App.tsx
+++ client/App.tsx
@@ -1,18 +1,18 @@
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomeScreen from "./Controllers/HomeScreenController";
+import TopBar from "./components/TopBar";
 import Button from '@mui/material/Button';
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Button>Hello plantpal!</Button>} />
-        <Route path="/home" element={<HomeScreen />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Button>Hello plantpal!</Button>}/>
+                <Route path="/home" element={<HomeScreen/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
