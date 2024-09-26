2009750014cfbffad82f03c0b9030bbb14a3421e
JuliaAdler <julia.l.adler@gmail.com>
2024-09-26 18:17:56 +0200

    created about page and added font family to theme

--- client/App.tsx
+++ client/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomeScreen from "./Controllers/HomeScreenController";
 import GeneralScreen from "./Controllers/GeneralScreenController";
+import AboutScreen from "./Controllers/AboutScreenController";
 import TopBar from "./components/TopBar";
 import Button from '@mui/material/Button';
 import AuthScreenController from "./Controllers/AuthScreenController";
@@ -18,6 +19,7 @@ const App: React.FC = () => {
                     <Route path="/home" element={<HomeScreen/>}/>
                     <Route path="/generalinfo" element={<GeneralScreen/>}/>
                     <Route path="/login" element={<AuthScreenController/>}></Route>
+                    <Route path="/about" element={<AboutScreen/>}/>
                 </Routes>
             </Router>
         </ThemeProvider>
---
+++ client/Controllers/AboutScreenController.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutScreenView from "../Views/AboutScreenView";
+
+type Props = {}
+
+export default function AboutScreenController({}: Props) {
+    return (
+        <AboutScreenView/>
+    )
+}
