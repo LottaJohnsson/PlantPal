53124974b2b5f6d74cad7ae72cb33b739af4c610
Charlotta Johnsson <chjohns@kth.se>
2024-09-29 21:16:59 +0200

    Connecting general info view to information from API calls

--- client/Controllers/GeneralScreenController.tsx
+++ client/Controllers/GeneralScreenController.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import GeneralScreenView from "../Views/GeneralScreenView";
+import {getCareAdvice, searchSpecies} from "../../server/Models/plantModel";
 
 type Props = {}
 
 export default function GeneralScreenController({}: Props) {
+    const [tabIndex, setTabIndex] = useState(0)
+    const [advice, setAdvice] = useState(null);
+    const [species, setSpecies] = useState(null);
+
+    function handleTabChange(event: React.SyntheticEvent, tabindex: number) {
+        setTabIndex(tabindex);
+    }
+
+    function onAddToProfile() {
+        console.log("Add to profile");
+        //TODO
+    }
+
+    useEffect(() => {
+        searchSpecies("lily").then(res => {
+            setSpecies(res[0]);
+        })
+        getCareAdvice("lily").then(res => {
+            setAdvice(res[0]);
+        })
+    }, []);
+
     return (
-        <GeneralScreenView/>
-    )
+        <>
+            <GeneralScreenView
+                advice={advice}
+                species={species}
+                handleTabChange={handleTabChange}
+                tabIndex={tabIndex}
+                onAddToProfile={onAddToProfile}/>
+        </>
+    );
 }
